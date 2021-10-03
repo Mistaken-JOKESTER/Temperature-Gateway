4f70146bcbec2ebefc651d7ea993042ef9270b37
Ankit Sharma <ankitsharmarock99@gmail.com>
2021-10-04 00:10:01 +0530

    mysql session

--- sessionSetup/sessionConfig.js
+++ sessionSetup/sessionConfig.js
@@ -1,16 +1,28 @@
 const session = require('express-session');
 const genuuid = require('./sessionUUID')
-const MongoDBStore = require('connect-mongodb-session')(session);
+const MySQLStore = require('express-mysql-session')(session)
 
-var store = new MongoDBStore({
-    uri: process.env.MONGO_URL,
-    collection: 'mySessions'
-  });
-
-  // Catch errors
-store.on('error', function(error) {
-    console.log(error);
-  });
+const sessionStore = new MySQLStore({
+  host     :process.env.DB_HOST,
+  port     :process.env.DB_PORT,
+  user     :process.env.DB_USER,
+  password :process.env.DB_PASSWORD,
+  database :process.env.DB_DATABASE,
+  clearExpired: true,
+checkExpirationInterval: 60 * 60 * 1000,
+expiration:  60 * 60 * 1000,
+createDatabaseTable: true,
+endConnectionOnClose: true,
+charset: 'utf8mb4_bin',
+  schema: {
+  tableName: 'sessions',
+  columnNames: {
+    session_id: 'sid',
+    expires: 'expires',
+    data: 'data'
+  }
+}
+})
 
 //Session Setup
 const sessionConfig= ({
@@ -18,7 +30,7 @@ const sessionConfig= ({
         return genuuid()
     },
     name:'sid',
-    store: store,
+    store: sessionStore,
     resave:false,
     secret: process.env.SESSION_SECRETE,
     resave: false,
